d055a3e6497953b4a2bcd31092560b471f24835b
dblanchard13 <davidb.prof@gmail.com>
2017-10-02 18:39:26 -0700

    Homework comments.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,6 @@
+// Would be good to put these variables inside the document.ready function so as to keep them off the global scope.
+// Also, it might be useful to store the questions/answers as objects so that it's easier to understand
+// what values are what.
 var questions_answers =[["Scotch is made in which country?", "Scotland","Ireland","America"],["Which of the following is not true for Bourbon", "Made in America", "At least 51% wheat", "Fermented with Bald Eagles tears"],["Whiskey is made with which of the following?","Love","Beer","Oranges"]];
 var responses = [];
 
@@ -7,9 +10,12 @@ $( document ).ready(function() {
 		time : 15,
 		reset: function() {
 			this.time = 15;
+			// totally fine to use native dom APIs over jQuery if you want,
+			// but I'd suggest that you keep it consistent across your code.
 			document.getElementById('seconds').innerHTML = this.time;
 		},
 		start: function() {
+			// since you aren't declaring counter anywhere, this variable ends up being placed on the global scope 😮
 			counter = setInterval(countdownTimer.count, 1000);	
 		},
 		stop: function() {
@@ -17,7 +23,8 @@ $( document ).ready(function() {
 		},
 		count: function() {
 				countdownTimer.time--;
-				console.log(countdownTimer.time);
+				// console.log is great for development, but try to keep them out of your production code.
+				// console.log(countdownTimer.time);
 			if (countdownTimer.time >= 0) {
 				$('#seconds').html(countdownTimer.time);
 			}
@@ -36,7 +43,7 @@ $( document ).ready(function() {
 	};
 
 function loadQuestion(questionSelection) {
-	console.log(questionSelection);
+	// console.log(questionSelection);
 	countdownTimer.reset();
   $("#questions").html(questions_answers[questionSelection][0]);
   $("#a").html(questions_answers[questionSelection][1]).show();
@@ -48,13 +55,16 @@ function getAnswer() {
 
 //  nextQuestion();
 	$('.answer').on('click', function() {
-	  console.log('alert', index);
+	  // console.log('alert', index);
 		index++;
-		console.log('click', index);
-		$("#questions").html('');
-		$("#a").html('');
-		$("#b").html('');
-		$("#c").html('');
+		// console.log('click', index);
+
+		// emptying out the html for the below elements is unnecessary since loadQuestions
+		// will be replacing the html anyways.
+		// $("#questions").html('');
+		// $("#a").html('');
+		// $("#b").html('');
+		// $("#c").html('');
 		loadQuestion();
 	})
 }
@@ -64,6 +74,8 @@ function showScore() {
 	var incorrect = 0;
 	var key_list = [1,3,2];
 	/*Count correct and incorrect answers*/
+	// By scoring this way, the user can only ever get the first 3 questions correct.
+	// So if a user restarts the quiz, their new answers will never get scored.
 	for(i=0; i<responses.length; i++) {
 		if(responses[i] === key_list[i]) {
 			correct += 1;
@@ -80,7 +92,7 @@ function showScore() {
 	$('#incorrect_answers').html("Incorrect")
 	$('#c').html('Good Job');	
 	$('#hidden').html("Drink more Whiskey")
-	console.log("Correct:" + correct);
+	// console.log("Correct:" + correct);
 	countdownTimer.stop();
 	$('#seconds').empty();
 	$('#start_button').show();
@@ -96,11 +108,13 @@ $('#start_button').on('click', function() {
 $('.answer').on('click', function() {
 	e = $(this).attr("value");
 	responses.push(parseInt(e));
-	console.log(e);
-	console.log(responses);
+	// console.log(e);
+	// console.log(responses);
  	index++;
  	if (index < questions_answers.length) {
- 	loadQuestion(index);
+ 		// nitpicky comment - try to keep your indentation consistent across your files as it makes it much easier to 
+ 		// parse through the code and understand what code is nested within certain blocks.
+	 	loadQuestion(index);
  	} else {
  		/*$(".answer").hide();*/
  		showScore();
